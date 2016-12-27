31e7d2e85a6e3ff1b6af7b9b46281393edfe7f1b
ycbai <ycbai@talend.com>
2016-12-27 17:05:51 +0800

    TUP-16491: NPE error when focus on jobname view then select Main tab,after convert spark job to standard

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/jobsettings/tabs/MainComposite.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/jobsettings/tabs/MainComposite.java
@@ -25,7 +25,6 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.fieldassist.ControlDecoration;
 import org.eclipse.jface.fieldassist.FieldDecoration;
 import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
@@ -72,6 +71,7 @@ import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.model.repository.RepositoryViewObject;
 import org.talend.core.repository.model.ItemReferenceBean;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.repository.seeker.RepositorySeekerManager;
 import org.talend.core.repository.ui.actions.DeleteActionCache;
 import org.talend.core.repository.ui.dialog.ItemReferenceDialog;
 import org.talend.core.repository.utils.ConvertJobsUtil;
@@ -82,12 +82,15 @@ import org.talend.core.runtime.CoreRuntimePlugin;
 import org.talend.core.ui.branding.IBrandingService;
 import org.talend.core.ui.editor.IJobEditorHandler;
 import org.talend.core.ui.editor.JobEditorHandlerManager;
+import org.talend.core.ui.properties.tab.HorizontalTabFactory;
+import org.talend.core.ui.properties.tab.TalendPropertyTabDescriptor;
 import org.talend.core.utils.KeywordsValidator;
 import org.talend.designer.core.ICamelDesignerCoreService;
 import org.talend.designer.core.i18n.Messages;
 import org.talend.repository.ProjectManager;
 import org.talend.repository.RepositoryWorkUnit;
 import org.talend.repository.model.IProxyRepositoryFactory;
+import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.RepositoryConstants;
 import org.talend.repository.ui.properties.StatusHelper;
 import org.talend.repository.ui.views.IJobSettingsView;
@@ -98,6 +101,8 @@ import org.talend.repository.ui.views.IJobSettingsView;
 public class MainComposite extends AbstractTabComposite {
 
     private boolean enableControl;
+
+    private HorizontalTabFactory tabFactory;
 
     private Text nameText;
 
@@ -135,9 +140,14 @@ public class MainComposite extends AbstractTabComposite {
 
     protected String statusLabelText = null;
 
+    public MainComposite(Composite parent, int style, HorizontalTabFactory tabFactory, IRepositoryViewObject obj) {
+        this(parent, style, tabFactory.getWidgetFactory(), obj);
+        this.tabFactory = tabFactory;
+    }
+
     /**
      * yzhang MainComposite constructor comment.
-     * 
+     *
      * @param parent
      * @param style
      */
@@ -664,6 +674,16 @@ public class MainComposite extends AbstractTabComposite {
                                                     if (isNewItemCreated) {
                                                         newRepositoryObject = proxyRepositoryFactory.getLastVersion(
                                                                 ProjectManager.getInstance().getCurrentProject(), newId);
+                                                        if (tabFactory != null) {
+                                                            IRepositoryNode newRepoViewNode = RepositorySeekerManager
+                                                                    .getInstance().searchRepoViewNode(newId);
+                                                            if (newRepoViewNode != null) {
+                                                                TalendPropertyTabDescriptor selection = tabFactory.getSelection();
+                                                                if (selection != null) {
+                                                                    selection.setData(newRepoViewNode.getObject());
+                                                                }
+                                                            }
+                                                        }
                                                     } else {
                                                         newRepositoryObject = repositoryObject;
                                                     }
