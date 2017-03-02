51665e58ab25d5ac974b42f5b7bec40106c8340f
cmeng-talend <cmeng@talend.com>
2017-03-02 19:01:55 +0800

    TUP-5280 feature: Studio use TIC as user management (additionaly to TAC) https://jira.talendforge.org/browse/TUP-5280

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginHelper.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginHelper.java
@@ -227,6 +227,7 @@ public class LoginHelper {
         if (connectionsBeans != storedConnections) {
             setStoredConnections(connectionsBeans);
         }
+        init();
     }
 
     public void saveLastConnectionBean(ConnectionBean connBean) {
@@ -234,6 +235,7 @@ public class LoginHelper {
         if (connBean != null) {
             lastConnection = connBean.getName();
         }
+        init();
     }
 
     public ConnectionBean getCurrentSelectedConnBean() {
@@ -249,6 +251,10 @@ public class LoginHelper {
 
     protected static ConnectionBean getConnection() {
         return LoginHelper.getInstance().getFirstConnBean();
+    }
+
+    public static boolean isRemoteConnection() {
+        return isRemoteConnection(getConnection());
     }
 
     protected static boolean needRestartForLocal(ConnectionBean curConnection) {
