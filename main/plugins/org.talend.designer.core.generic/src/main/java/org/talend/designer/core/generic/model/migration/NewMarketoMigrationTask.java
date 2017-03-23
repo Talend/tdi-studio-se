b220335b216ec526ee371eb9fd196d55fa4eb78e
undx <emmanuel.gallois@gmail.com>
2017-03-23 10:27:54 +0000

    feature(TDI-38379): initial commit for tMarketoComponents Migration Task

---
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/NewMarketoMigrationTask.java
@@ -0,0 +1,43 @@
+package org.talend.designer.core.generic.model.migration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+
+import org.talend.designer.core.generic.utils.ParameterUtilTool;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+
+public class NewMarketoMigrationTask extends NewComponentFrameworkMigrationTask {
+    @Override
+    public Date getOrder() {
+        return new GregorianCalendar(2017, 3, 8, 10, 0, 0).getTime();
+    }
+
+    @Override
+    protected Properties getPropertiesFromFile() {
+        Properties props = new Properties();
+        InputStream in = getClass().getResourceAsStream("NewMarketoMigrationTask.properties");//$NON-NLS-1$
+        try {
+            props.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return props;
+    }
+
+    @Override
+    protected ElementParameterType getParameterType(NodeType node, String paramName) {
+        ElementParameterType paramType = ParameterUtilTool.findParameterType(node, paramName);
+        //
+        if (node != null && paramType != null) {
+            Object value = ParameterUtilTool.convertParameterValue(paramType);
+            // connection.apiMode USE_REST_API USE_SOAP_API
+        }
+        return paramType;
+    }
+}
