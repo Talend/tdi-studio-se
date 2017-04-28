9cbde750a3cd33c1897150a5f635abcb18b7d087
Dmytro Sylaiev <dsylaiev@talend.com>
2017-04-28 11:07:22 +0300

    style(TDI-38441): Fix indentation

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddFailOnErrorOnTFileCopyTDI38441.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddFailOnErrorOnTFileCopyTDI38441.java
@@ -19,55 +19,53 @@ import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
 
 public class AddFailOnErrorOnTFileCopyTDI38441 extends AbstractJobMigrationTask {
 
-	@Override
-	public Date getOrder() {
-		GregorianCalendar gc = new GregorianCalendar(2017, 4, 27, 14, 0, 0);
-		return gc.getTime();
-	}
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2017, 4, 27, 14, 0, 0);
+        return gc.getTime();
+    }
 
-	@Override
-	public ExecutionResult execute(Item item) {
-		ProcessType processType = getProcessType(item);
+    @Override
+    public ExecutionResult execute(Item item) {
+        ProcessType processType = getProcessType(item);
         if (getProject().getLanguage() != ECodeLanguage.JAVA || processType == null) {
             return ExecutionResult.NOTHING_TO_DO;
         }
         try {
-            String componentsName = new String ("tFileCopy"); //$NON-NLS-1$
+            String componentsName = new String("tFileCopy"); //$NON-NLS-1$
 
             IComponentFilter filter = new NameComponentFilter(componentsName);
             IComponentConversion addOption = new IComponentConversion() {
 
-				@Override
-				public void transform(NodeType node) {
-					
-					String propertyType = "CHECK"; //$NON-NLS-1$
-					String propertyName = "FAILON"; //$NON-NLS-1$
-					String isCopyDirectoryPropertyName = "ENABLE_COPY_DIRECTORY"; //$NON-NLS-1$
-					
-					if (ComponentUtilities.getNodeProperty(node, propertyName) == null) {
-		                ComponentUtilities.addNodeProperty(node, propertyName, propertyType);
-		            }
-		            ElementParameterType copyingDirectory = ComponentUtilities.getNodeProperty(node, isCopyDirectoryPropertyName);
-		            if (copyingDirectory != null) {
-		            	if ("true".equals(copyingDirectory.getValue())) { //$NON-NLS-1$
-		            		ComponentUtilities.setNodeValue(node, propertyName, "false"); //$NON-NLS-1$
-		            		return;
-		            	}
-		            	ComponentUtilities.setNodeValue(node, propertyName, "true"); //$NON-NLS-1$
-		            }
-					
-				}
-            	
+                @Override
+                public void transform(NodeType node) {
+
+                    String propertyType = "CHECK"; //$NON-NLS-1$
+                    String propertyName = "FAILON"; //$NON-NLS-1$
+                    String isCopyDirectoryPropertyName = "ENABLE_COPY_DIRECTORY"; //$NON-NLS-1$
+
+                    if (ComponentUtilities.getNodeProperty(node, propertyName) == null) {
+                        ComponentUtilities.addNodeProperty(node, propertyName, propertyType);
+                    }
+                    ElementParameterType copyingDirectory = ComponentUtilities.getNodeProperty(node, isCopyDirectoryPropertyName);
+                    if (copyingDirectory != null) {
+                        if ("true".equals(copyingDirectory.getValue())) { //$NON-NLS-1$
+                            ComponentUtilities.setNodeValue(node, propertyName, "false"); //$NON-NLS-1$
+                            return;
+                        }
+                        ComponentUtilities.setNodeValue(node, propertyName, "true"); //$NON-NLS-1$
+                    }
+
+                }
+
             };
-            ModifyComponentsAction
-            	.searchAndModify(item, processType, filter, Arrays.<IComponentConversion> asList(addOption));
+            ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays.<IComponentConversion> asList(addOption));
             return ExecutionResult.SUCCESS_NO_ALERT;
         } catch (Exception e) {
             ExceptionHandler.process(e);
             return ExecutionResult.FAILURE;
         }
-    
-        
-	}
-	
+
+    }
+
 }
