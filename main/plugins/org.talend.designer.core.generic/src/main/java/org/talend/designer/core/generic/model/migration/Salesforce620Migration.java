ff4b7c157e138cc1d565fadf9a98beab83d18846
pyzhou-talend <pyzhou@talend.com>
2017-11-23 10:25:48 +0800

    fix(TDI-39711):remove \r|\n|\r\n of module name.

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/Salesforce620Migration.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/Salesforce620Migration.java
@@ -129,7 +129,7 @@ public class Salesforce620Migration extends AbstractJobMigrationTask {
                 NamedThing nt = newProperties.getProperty("module.moduleName");
                 if (nt != null && nt instanceof Property) {
                     Property moduleNameProperty = (Property) nt;
-                    String moduleName = (String) moduleNameProperty.getValue();
+                    String moduleName = ((String) moduleNameProperty.getValue()).replaceAll("\r|\n|\r\n", "");
                     if (ContextParameterUtils.isContainContextParam(moduleName)) {
                         moduleName = TalendQuoteUtils.removeQuotes(moduleName);
                     } else {
