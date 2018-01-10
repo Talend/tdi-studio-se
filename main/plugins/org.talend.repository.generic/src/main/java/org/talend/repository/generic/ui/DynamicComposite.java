12216c4827182fa24192154c8dbf88dc1617813f
wang wei <wwang@talend.com>
2018-01-10 16:43:32 +0800

    fix(TUP-19235): revert something to make the refresh for advanced page works

--- main/plugins/org.talend.repository.generic/src/main/java/org/talend/repository/generic/ui/DynamicComposite.java
+++ main/plugins/org.talend.repository.generic/src/main/java/org/talend/repository/generic/ui/DynamicComposite.java
@@ -418,6 +418,7 @@ public class DynamicComposite extends MissingSettingsMultiThreadDynamicComposite
     }
 
     private void reset(boolean refresh) {
+        resetParameters();
         if (refresh) {
             Display.getDefault().asyncExec(new Runnable() {
 
@@ -426,14 +427,11 @@ public class DynamicComposite extends MissingSettingsMultiThreadDynamicComposite
                     refresh();
                 }
             });
-        }else{
-            resetParameters();
         }
     }
 
     @Override
     public void refresh() {
-        resetParameters();
         if (element instanceof FakeElement) {
             DisplayUtils.getDisplay().syncExec(new Runnable() {
 
