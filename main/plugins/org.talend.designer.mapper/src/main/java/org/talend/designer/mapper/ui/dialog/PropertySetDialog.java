0bad03e0bd4e02d851a8df8b4d80d6314c82cb0c
xlwang <xlwang@BJ-XLWang.talend.com>
2018-01-31 16:51:30 +0800

    fix(TUP-19278)Auto-Map algorithm settings not readable\ https://jira.talendforge.org/browse/TUP-19278

--- main/plugins/org.talend.designer.mapper/src/main/java/org/talend/designer/mapper/ui/dialog/PropertySetDialog.java
+++ main/plugins/org.talend.designer.mapper/src/main/java/org/talend/designer/mapper/ui/dialog/PropertySetDialog.java
@@ -22,7 +22,6 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
