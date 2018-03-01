19829083767daee9acdc291cabfd0f7616af6c7c
hwang <hwang@talend.com>
2018-03-01 10:15:54 +0800

    fix(TUP-19421):Missing connection info when D&D tJDBCSCDELT and tELTJDBCMap

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
@@ -717,8 +717,7 @@ public class ChangeValuesFromRepository extends ChangeMetadataCommand {
                 String databaseType = ((DatabaseConnection) connection).getDatabaseType();
                 if ("JDBC".equals(databaseType)) {
                     IComponent component = node.getComponent();
-                    if (!ComponentCategory.CATEGORY_4_DI.getName().equals(component.getPaletteType())
-                            && component.getName().startsWith("tJDBC")) {
+                    if (component.getName().startsWith("tJDBC") || component.getName().startsWith("tELTJDBC")) {
                         if (EParameterName.URL.getName().equals(paramName)) {
                             return "connection.jdbcUrl";
                         } else if (EParameterName.DRIVER_JAR.getName().equals(paramName)) {
