d1ff1e156137f77c4fe244e21e26b841cac22e88
Dmytro Sylaiev <dsylaiev@talend.com>
2018-05-02 18:01:03 +0300

    chore(TDI-40186): Refactoring migration task

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/KeepChangeRemoteDirFalseForFTPFileExist.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/KeepChangeRemoteDirFalseForFTPFileExist.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
 import org.talend.core.language.ECodeLanguage;
 import org.talend.core.model.components.ComponentUtilities;
 import org.talend.core.model.components.ModifyComponentsAction;
@@ -19,6 +20,7 @@ import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
 public class KeepChangeRemoteDirFalseForFTPFileExist extends AbstractJobMigrationTask {
     private final static String PROPERTY_TYPE = "CHECK"; //$NON-NLS-1$
     private final static String PROPERTY_NAME = "MOVE_TO_THE_CURRENT_DIRECTORY"; //$NON-NLS-1$
+    private final static String COMPONENT_NAME = "tFTPFileExist"; //$NON-NLS-1$
     
     @Override
     public Date getOrder() {
@@ -32,26 +34,27 @@ public class KeepChangeRemoteDirFalseForFTPFileExist extends AbstractJobMigratio
         if (getProject().getLanguage() != ECodeLanguage.JAVA || processType == null) {
             return ExecutionResult.NOTHING_TO_DO;
         }
-        try {
-            String componentsName = new String("tFTPFileExist"); //$NON-NLS-1$
+        
+        String componentsName = new String(COMPONENT_NAME);
 
-            IComponentFilter filter = new NameComponentFilter(componentsName);
-            IComponentConversion addOption = new IComponentConversion() {
+        IComponentFilter filter = new NameComponentFilter(componentsName);
+        IComponentConversion addOption = new IComponentConversion() {
 
-                @Override
-                public void transform(NodeType node) {
+            @Override
+            public void transform(NodeType node) {
 
-                    if (ComponentUtilities.getNodeProperty(node, PROPERTY_NAME) == null) {
-                        ComponentUtilities.addNodeProperty(node, PROPERTY_NAME, PROPERTY_TYPE);
-                    }
-                    //set false to the value after migration
-                    ComponentUtilities.setNodeValue(node, PROPERTY_NAME, "false"); //$NON-NLS-1$
+                if (ComponentUtilities.getNodeProperty(node, PROPERTY_NAME) == null) {
+                    ComponentUtilities.addNodeProperty(node, PROPERTY_NAME, PROPERTY_TYPE);
                 }
+                //set false to the value after migration
+                ComponentUtilities.setNodeValue(node, PROPERTY_NAME, "false"); //$NON-NLS-1$
+            }
 
-            };
+        };
+        try {
             ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays.<IComponentConversion> asList(addOption));
             return ExecutionResult.SUCCESS_NO_ALERT;
-        } catch (Exception e) {
+        } catch (PersistenceException e) {
             ExceptionHandler.process(e);
             return ExecutionResult.FAILURE;
         }
