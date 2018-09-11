d08c604a0adc70398f53c39b71de4c2537606c50
Ivan Honchar <ivan.honchar@globallogic.com>
2018-09-11 23:30:23 +0300

    fix(TCOMP-1039): refactor converting table value from action and fix integer issue

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/action/update/UpdateCommand.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/action/update/UpdateCommand.java
@@ -12,19 +12,15 @@
 // ============================================================================
 package org.talend.sdk.component.studio.model.action.update;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
 import org.talend.core.model.process.EParameterFieldType;
-import org.talend.designer.core.model.components.ElementParameter;
 import org.talend.sdk.component.studio.model.action.Action;
 import org.talend.sdk.component.studio.model.parameter.ButtonParameter;
 import org.talend.sdk.component.studio.model.parameter.TaCoKitElementParameter;
+import org.talend.sdk.component.studio.model.parameter.TableElementParameter;
 import org.talend.sdk.component.studio.model.parameter.command.BaseAsyncAction;
 
 /**
@@ -51,7 +47,7 @@ public class UpdateCommand extends BaseAsyncAction<Object> {
     public UpdateCommand(final Action<Object> action, final String basePath,
                          final List<TaCoKitElementParameter> parameters, final ButtonParameter button) {
         super(action);
-        this.basePath = basePath;
+        this.basePath = basePath + ".";
         this.parameters = Collections.unmodifiableList(parameters);
         this.button = button;
     }
@@ -64,36 +60,12 @@ public class UpdateCommand extends BaseAsyncAction<Object> {
     @Override
     protected void onResult(Map<String, Object> result) {
         parameters.forEach(p -> {
-            final String key = p.getName().replaceFirst(basePath + ".", "");
+            final String key = p.getName().replaceFirst(basePath, "");
             final Object value = result.get(key);
             if (value != null) {
                 if (EParameterFieldType.TABLE.equals(p.getFieldType())) {
-                    final List<Map<String, Object>> fixedValue = new ArrayList<>();
-                    List<Object> originalValue = (List<Object>) value;
-                    for (Object o : originalValue) {
-                        if (o instanceof Map) {
-                            Map<String, Object> row = new LinkedHashMap<>();
-                            Object[] childrenParameters = p.getListItemsValue();
-                            for (Object childParam : childrenParameters) {
-                                ElementParameter param = (ElementParameter) childParam;
-                                String columnKey = param.getName().replaceFirst(Pattern.quote(p.getName() + "[]."), "");
-                                Object columnValue = ((Map<String, Object>) o).get(columnKey);
-                                row.put(param.getName(), columnValue);
-                            }
-                            fixedValue.add(row);
-                        } else {
-                            Map<String, Object> row = new LinkedHashMap<>();
-                            Object[] childrenParameters = p.getListItemsValue();
-                            if (childrenParameters.length != 1) {
-                                throw new IllegalStateException("Table parameter has different than 1 number of columns");
-                            }
-                            TaCoKitElementParameter childParameter = (TaCoKitElementParameter) childrenParameters[0];
-                            String columnKey = childParameter.getName();
-                            row.put(columnKey, o);
-                            fixedValue.add(row);
-                        }
-                    }
-                    p.setValue(fixedValue);
+                    TableElementParameter t = (TableElementParameter) p;
+                    t.setValue(t.fromAction((List<Object>) value));
                 } else {
                     p.setValue(value);
                 }
