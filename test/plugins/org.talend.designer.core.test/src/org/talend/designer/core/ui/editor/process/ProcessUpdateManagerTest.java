7a6188fff319667e7b6742223a32061a0b25f889
zhangjie <jie.zhang@talend.com>
2018-12-04 14:47:31 +0800

    add junit(TUP-21094)

--- test/plugins/org.talend.designer.core.test/src/org/talend/designer/core/ui/editor/process/ProcessUpdateManagerTest.java
+++ test/plugins/org.talend.designer.core.test/src/org/talend/designer/core/ui/editor/process/ProcessUpdateManagerTest.java
@@ -5,11 +5,9 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.talend.core.GlobalServiceRegister;
 import org.talend.core.database.EDatabaseTypeName;
 import org.talend.core.model.components.ComponentCategory;
 import org.talend.core.model.components.IComponent;
-import org.talend.core.model.components.IComponentsService;
 import org.talend.core.model.metadata.builder.connection.ConnectionFactory;
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
 import org.talend.core.model.properties.PropertiesFactory;
@@ -22,34 +20,39 @@ public class ProcessUpdateManagerTest {
 
     private ProcessUpdateManager updateManager;
 
-    private IComponentsService compService;
-
-
     @Before
     public void setUp() throws Exception {
         Property property = PropertiesFactory.eINSTANCE.createProperty();
         process = new Process(property);
         updateManager = new ProcessUpdateManager(process);
-        compService = (IComponentsService) GlobalServiceRegister.getDefault()
-                .getService(IComponentsService.class);
     }
 
     @Test
     public void testIsOldJDBC() throws Exception {
-        IComponent com = null;
+        IComponent component = null;
         Node node = null;
         DatabaseConnection connection = ConnectionFactory.eINSTANCE.createDatabaseConnection();
 
-        com = ComponentsFactoryProvider.getInstance().get("tELTMap", ComponentCategory.CATEGORY_4_DI.getName());
-        node = new Node(com, process);
+        component = ComponentsFactoryProvider.getInstance().get("tELTMap", ComponentCategory.CATEGORY_4_DI.getName());
+        node = new Node(component, process);
         connection.setDatabaseType(EDatabaseTypeName.GENERAL_JDBC.getProduct());
         assertTrue(updateManager.isOldJDBC(node, connection));
 
         connection.setDatabaseType(EDatabaseTypeName.MYSQL.getProduct());
         assertFalse(updateManager.isOldJDBC(node, connection));
 
-        com = ComponentsFactoryProvider.getInstance().get("tSqoopMerge", ComponentCategory.CATEGORY_4_DI.getName());
-        node = new Node(com, process);
+        component = ComponentsFactoryProvider.getInstance().get("tSqoopMerge", ComponentCategory.CATEGORY_4_DI.getName());
+        node = new Node(component, process);
+        connection.setDatabaseType(EDatabaseTypeName.GENERAL_JDBC.getProduct());
+        assertTrue(updateManager.isOldJDBC(node, connection));
+
+        component = ComponentsFactoryProvider.getInstance().get("tJDBCInput", ComponentCategory.CATEGORY_4_DI.getName());
+        node = new Node(component, process);
+        connection.setDatabaseType(EDatabaseTypeName.GENERAL_JDBC.getProduct());
+        assertFalse(updateManager.isOldJDBC(node, connection));
+
+        component = ComponentsFactoryProvider.getInstance().get("tJDBCInput", ComponentCategory.CATEGORY_4_MAPREDUCE.getName());
+        node = new Node(component, process);
         connection.setDatabaseType(EDatabaseTypeName.GENERAL_JDBC.getProduct());
         assertTrue(updateManager.isOldJDBC(node, connection));
 
