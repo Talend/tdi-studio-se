272ca23d8b6a17e84597106527ba57b2276e853c
Dmytro Sylaiev <dmytro.sylaiev@globallogic.com>
2019-01-04 16:57:17 +0200

    feat(TDI-40990): Extract SSLSessionReuseFTPSClient behavior

--- main/plugins/org.talend.designer.components.libs/libs_src/commons-net-ftps-proxy/src/main/java/org/apache/commons/net/ftp/FTPSHTTPClient.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/commons-net-ftps-proxy/src/main/java/org/apache/commons/net/ftp/FTPSHTTPClient.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class FTPSHTTPClient extends FTPSClient {
+public class FTPSHTTPClient extends SSLSessionReuseFTPSClient {
 
     private final String proxyHost;
 
@@ -155,29 +155,6 @@ public class FTPSHTTPClient extends FTPSClient {
         }
 
         return socket;
-    }
-
-    @Override
-    protected void _prepareDataSocket_(final Socket socket) {
-        if (socket instanceof SSLSocket) {
-            final SSLSession session = ((SSLSocket) _socket_).getSession();
-            final SSLSessionContext context = session.getSessionContext();
-            try {
-                final Field sessionHostPortCache = context.getClass().getDeclaredField("sessionHostPortCache");
-                sessionHostPortCache.setAccessible(true);
-                final Object cache = sessionHostPortCache.get(context);
-                final Method putMethod = cache.getClass().getDeclaredMethod("put", Object.class, Object.class);
-                putMethod.setAccessible(true);
-                final Method getHostMethod = socket.getClass().getDeclaredMethod("getHost");
-                getHostMethod.setAccessible(true);
-                Object host = getHostMethod.invoke(socket);
-                final String key =
-                        String.format("%s:%s", host, String.valueOf(socket.getPort())).toLowerCase(Locale.ROOT);
-                putMethod.invoke(cache, key, session);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     /**
---
+++ main/plugins/org.talend.designer.components.libs/libs_src/commons-net-ftps-proxy/src/main/java/org/apache/commons/net/ftp/SSLSessionReuseFTPSClient.java
@@ -0,0 +1,40 @@
+package org.apache.commons.net.ftp;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSessionContext;
+import javax.net.ssl.SSLSocket;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.Socket;
+import java.util.Locale;
+
+public class SSLSessionReuseFTPSClient extends FTPSClient {
+
+    public SSLSessionReuseFTPSClient(boolean isImplicit, SSLContext context) {
+        super(isImplicit, context);
+    }
+
+    @Override
+    protected void _prepareDataSocket_(final Socket socket) {
+        if (socket instanceof SSLSocket) {
+            final SSLSession session = ((SSLSocket) _socket_).getSession();
+            final SSLSessionContext context = session.getSessionContext();
+            try {
+                final Field sessionHostPortCache = context.getClass().getDeclaredField("sessionHostPortCache");
+                sessionHostPortCache.setAccessible(true);
+                final Object cache = sessionHostPortCache.get(context);
+                final Method putMethod = cache.getClass().getDeclaredMethod("put", Object.class, Object.class);
+                putMethod.setAccessible(true);
+                final Method getHostMethod = socket.getClass().getDeclaredMethod("getHost");
+                getHostMethod.setAccessible(true);
+                Object host = getHostMethod.invoke(socket);
+                final String key =
+                        String.format("%s:%s", host, String.valueOf(socket.getPort())).toLowerCase(Locale.ROOT);
+                putMethod.invoke(cache, key, session);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
