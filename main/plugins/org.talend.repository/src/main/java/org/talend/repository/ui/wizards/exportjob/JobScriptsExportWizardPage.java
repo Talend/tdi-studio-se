3f53119cd2117700b56581df24a4eeee7f6a84d5
zhangjie <jie.zhang@talend.com>
2019-04-03 10:28:36 +0800

    fix(TUP-22395)Shouldn't be overwrite button for creating unexist directory for build job

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/JobScriptsExportWizardPage.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/JobScriptsExportWizardPage.java
@@ -1269,8 +1269,7 @@ public abstract class JobScriptsExportWizardPage extends WizardFileSystemResourc
     @Override
     protected boolean queryYesNoQuestion(String message) {
         MessageDialog dialog = new MessageDialog(getContainer().getShell(), IDEWorkbenchMessages.Question, (Image) null, message,
-                MessageDialog.NONE, 0, IDialogConstants.YES_LABEL,
-                IDialogConstants.CANCEL_LABEL) { // $NON-NLS-1$
+                MessageDialog.NONE, 0, IDialogConstants.YES_LABEL, IDialogConstants.CANCEL_LABEL) {
 
             @Override
             protected int getShellStyle() {
