03423b90d26b87a1d5a152e6c7ee8ea370c397e9
dmytroshekera <dshekera@talend.com>
2019-07-16 16:10:12 +0300

    bugfix(TDI-42581): tSOAP doesn't support SOAPActions for Soap 1.2

--- main/plugins/org.talend.designer.components.libs/libs_src/soap/src/main/java/org/talend/soap/SOAPUtil.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/soap/src/main/java/org/talend/soap/SOAPUtil.java
@@ -125,12 +125,7 @@ public class SOAPUtil {
         SOAPMessage message = messageFactory.createMessage();
         MimeHeaders mimeHeaders = message.getMimeHeaders();
 
-        // https://jira.talendforge.org/browse/TDI-42581 skip add SOAPAction directly to header v1.2
-        if (SOAP12.equals(version)) {
-            mimeHeaders.addHeader("Content-Type", "application/soap+_xml; charset=utf-8; action=\"" + soapAction+"\"");
-        } else {
-            mimeHeaders.setHeader("SOAPAction", soapAction);
-        }
+        setSoapAction(version, soapAction, mimeHeaders);
 
         if (basicAuth) {
             addBasicAuthHeader(mimeHeaders, username, password);
@@ -270,13 +265,8 @@ public class SOAPUtil {
     	}
     	SOAPMessage message = messageFactory.createMessage();
     	MimeHeaders mimeHeaders = message.getMimeHeaders();
-        if (SOAP12.equals(version)) {
-            // https://jira.talendforge.org/browse/TDI-42581 skip add SOAPAction directly to header
-            mimeHeaders.addHeader("Content-Type", "application/soap+_xml; charset=utf-8; action=\"" + soapAction+"\"");
-        } else {
-            mimeHeaders.setHeader("SOAPAction", soapAction);
-        }
-    	SOAPPart soapPart = message.getSOAPPart();
+        setSoapAction(version, soapAction, mimeHeaders);
+        SOAPPart soapPart = message.getSOAPPart();
     	
     	String encoding = getEncoding(soapMessage);
     	
@@ -303,8 +293,18 @@ public class SOAPUtil {
 			return null;
 		}
     }
-	
-	private String getEncoding(String text) {
+
+    /* https://jira.talendforge.org/browse/TDI-42581 skip add SOAPAction directly to header v1.2 */
+    private void setSoapAction(String version, String soapAction, MimeHeaders mimeHeaders) {
+        if (SOAP12.equals(version)) {
+            // https://jira.talendforge.org/browse/TDI-42581 skip add SOAPAction directly to header
+            mimeHeaders.setHeader("Content-Type", "application/soap+_xml; charset=utf-8; action=\"" + soapAction + "\"");
+        } else {
+            mimeHeaders.setHeader("SOAPAction", soapAction);
+        }
+    }
+
+    private String getEncoding(String text) {
         String result = Charset.defaultCharset().name();
 
         if(text == null) {
