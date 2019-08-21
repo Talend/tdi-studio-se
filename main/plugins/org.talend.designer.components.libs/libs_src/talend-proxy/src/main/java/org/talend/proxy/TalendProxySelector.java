5b10c20b65eb014de4c84958103a9d53da92b91f
Dmytro Sylaiev <dmytro.sylaiev@globallogic.com>
2019-08-21 15:56:17 +0300

    feat(TDI-40977): Add posibility to use all-threads setting

--- main/plugins/org.talend.designer.components.libs/libs_src/talend-proxy/src/main/java/org/talend/proxy/TalendProxySelector.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-proxy/src/main/java/org/talend/proxy/TalendProxySelector.java
@@ -16,7 +16,8 @@ public class TalendProxySelector extends ProxySelector {
 
     private static final Logger log = Logger.getLogger(TalendProxySelector.class);
 
-    private ThreadLocal<ProxyHolder> myHolder;
+    private ThreadLocal<ProxyHolder> threadLocalProxyHolder;
+    private ProxyHolder globalProxyHolder;
 
     public static synchronized TalendProxySelector getInstance() {
         if (instance == null) {
@@ -27,27 +28,36 @@ public class TalendProxySelector extends ProxySelector {
     }
 
     private TalendProxySelector() {
+        globalProxyHolder = new ProxyHolder();
     }
 
-    public void addProxySettingsForThread(Proxy proxy, String... hosts) {
-        if (myHolder == null) {
-            myHolder = new ThreadLocal<>();
-        }
-        if (myHolder.get() == null) {
-            ProxyHolder newProxyHolder = new ProxyHolder();
-            myHolder.set(newProxyHolder);
-        }
+    public void addProxySettingsForThread(Proxy proxy, boolean forAllThreads, String... hosts) {
+        if (forAllThreads) {
+            globalProxyHolder.putNewHosts(proxy, hosts);
+        } else {
+            if (threadLocalProxyHolder == null) {
+                threadLocalProxyHolder = new ThreadLocal<>();
+            }
+            if (threadLocalProxyHolder.get() == null) {
+                ProxyHolder newProxyHolder = new ProxyHolder();
+                threadLocalProxyHolder.set(newProxyHolder);
+            }
 
-        myHolder.get().putNewHosts(proxy, hosts);
+            threadLocalProxyHolder.get().putNewHosts(proxy, hosts);
+        }
     }
 
     @Override
     public List<Proxy> select(URI uri) {
         String uriString = uri.getHost() + ":" + uri.getPort();
         log.debug("Network request hadling from Talend proxy selector. Thread " + Thread.currentThread().getName() + ". URI to connect: " + uriString);
-        if (myHolder != null && myHolder.get() != null && myHolder.get().getProxyMap().containsKey(uri.toString())) {
-            log.debug("Proxy " + myHolder.get().getProxyMap().get(uriString) + " is using to connect to URI " + uriString);
-            return Collections.singletonList(myHolder.get().getProxyMap().get(uriString));
+        if (globalProxyHolder.getProxyMap().containsKey(uriString)) {
+            log.debug("All threads proxy " + globalProxyHolder.getProxyMap().get(uriString) + " is using to connect to URI " + uriString);
+            return Collections.singletonList(globalProxyHolder.getProxyMap().get(uriString));
+        } else if (threadLocalProxyHolder != null && threadLocalProxyHolder.get() != null && threadLocalProxyHolder.get().getProxyMap().containsKey(uriString))
+        {
+            log.debug("Proxy " + threadLocalProxyHolder.get().getProxyMap().get(uriString) + " is using to connect to URI " + uriString);
+            return Collections.singletonList(threadLocalProxyHolder.get().getProxyMap().get(uriString));
         } else {
             log.debug("No proxy is using to connect to URI " + uriString);
             return Collections.singletonList(Proxy.NO_PROXY);
