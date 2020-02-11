f674c48a2284be4f1f843496981a3f75d29c9a3c
kjwang <kjwang@talend.com>
2020-02-11 09:27:59 +0800

    Fix: TUP-25943 tELTMap generated query has extra symbols generated (#4415) (#4424)
    * Fix: TUP-25943 tELTMap generated query has extra symbols generated
    https://jira.talendforge.org/browse/TUP-25943
    
    Conflicts:
    	main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/DbGenerationManager.java

--- main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/DbGenerationManager.java
+++ main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/DbGenerationManager.java
@@ -1474,8 +1474,8 @@ public abstract class DbGenerationManager {
                                             oriName = oriName.replaceAll("\\$", "\\\\\\$"); //$NON-NLS-1$ //$NON-NLS-2$
                                         }
                                         expression = expression.replaceFirst(tableValue + "\\." + co.getLabel(), //$NON-NLS-1$
-                                        		tableValue + "\\." + oriName); //$NON-NLS-1$
-                                        expression = replaceAuotes(expression, quto_markParser, quto_mark);
+                                                tableValue + "\\." + oriName); //$NON-NLS-1$
+                                        expression = replaceAuotes(component, expression, quto_markParser, quto_mark);
                                     }
                                 }
 
@@ -1490,11 +1490,14 @@ public abstract class DbGenerationManager {
         return expression;
     }
 
-    protected String replaceAuotes(String expression, String quoParser, String quote){
+    protected String replaceAuotes(DbMapComponent component, String expression, String quoParser, String quote) {
         if(!expression.contains("'")){
-            return expression.replaceAll(quoParser,"\\\\" +quote); //$NON-NLS-1$
+            if (isComplexExpression(component, expression)) {
+                return expression;
+            }
+            return expression.replaceAll(quoParser,"\\\\" +quote); //$NON-NLS-1$;
         }
-
+        
         List<Integer> indexs = new ArrayList<>();
         for(int i=0;i<expression.length();i++){
             if("'".equals(String.valueOf(expression.charAt(i)))){
@@ -1525,6 +1528,13 @@ public abstract class DbGenerationManager {
             }
         }
         return result.toString();
+    }
+    
+    protected boolean isComplexExpression(DbMapComponent component, String expression) {
+        if (parser.isContainsGlobalMapExpression(expression)) {
+            return true;
+        }
+        return false;
     }
 
     private String getQuote(DbMapComponent component){
