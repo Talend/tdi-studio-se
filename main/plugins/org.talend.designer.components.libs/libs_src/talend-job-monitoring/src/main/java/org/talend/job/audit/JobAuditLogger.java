ce3574096f568884e05c5b3bbcf3c042bdc84fec
wwang-talend <wwang@talend.com>
2020-06-02 15:30:59 +0800

    fix(TDI-44172): Observability: Create new "component execution" event and emit it frequently

--- main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobAuditLogger.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobAuditLogger.java
@@ -6,20 +6,23 @@ import org.talend.logging.audit.EventAuditLogger;
 import org.talend.logging.audit.LogLevel;
 
 public interface JobAuditLogger extends EventAuditLogger {
-	
-	@AuditEvent(category = "job", message = "Job start : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, timestamp:{timestamp}", level = LogLevel.INFO)
-    void jobstart(Context context);
-	
-	@AuditEvent(category = "job", message = "Job stop : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, timestamp:{timestamp}, status:{status}, duration:{duration}", level = LogLevel.INFO)
-    void jobstop(Context context);
-	
-	@AuditEvent(category = "connector", message = "Component run : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, connector_type:{connectorType}, connector_id:{connectorId}", level = LogLevel.INFO)
-    void runcomponent(Context context);
 
-    @AuditEvent(category = "connection", message = "Component {connectorType} {connectionType} {rows} rows in {duration} with {connectionName} line", level = LogLevel.INFO)
-    void flowOutput(Context context);
-    
-    @AuditEvent(category = "connection", message = "Component {connectorType} received {rows} rows in {duration} with {connectionName} line", level = LogLevel.INFO)
-    void flowInput(Context context);
+	@AuditEvent(category = "jobStart", message = "Job start : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, timestamp:{timestamp}", level = LogLevel.INFO)
+	void jobstart(Context context);
+
+	@AuditEvent(category = "jobStop", message = "Job stop : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, timestamp:{timestamp}, status:{status}, duration:{duration}", level = LogLevel.INFO)
+	void jobstop(Context context);
+
+	@AuditEvent(category = "runComponent", message = "Component run : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, connector_type:{connectorType}, connector_id:{connectorId}, connector_label:{connectorLabel}", level = LogLevel.INFO)
+	void runcomponent(Context context);
+
+	@AuditEvent(category = "flowOutput", message = "Component {connectorType} {connectorLabel} {connectionType} {rows} rows in {duration} with {connectionName} line", level = LogLevel.INFO)
+	void flowOutput(Context context);
+
+	@AuditEvent(category = "flowInput", message = "Component {connectorType} {connectorLabel} received {rows} rows in {duration} with {connectionName} line", level = LogLevel.INFO)
+	void flowInput(Context context);
+
+	@AuditEvent(category = "flowExecution", message = "connection : {connectionName}, row : {rows}, cost : {duration}", level = LogLevel.INFO)
+	void flowExecution(Context context);
 
 }
