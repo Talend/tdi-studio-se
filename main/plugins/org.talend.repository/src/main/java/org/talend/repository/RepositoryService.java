b723a89dafafe7917c926a3368afba44d649832c
Chao MENG <cmeng@talend.com>
2020-08-03 01:04:46 +0800

    workitem(TUP-28063): Improve studio bundles reloading logic https://jira.talendforge.org/browse/TUP-28063

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/RepositoryService.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/RepositoryService.java
@@ -327,6 +327,7 @@ public class RepositoryService implements IRepositoryService, IRepositoryContext
 
     private boolean isloginDialogDisabled() {
         boolean reload = Boolean.parseBoolean(System.getProperty("talend.project.reload")); //$NON-NLS-1$
+        reload = reload || Boolean.getBoolean("talend.project.reload4Proj");
         PreferenceManipulator preferenceManipulator = new PreferenceManipulator();
         ConnectionBean lastBean = null;
         if (reload) {
