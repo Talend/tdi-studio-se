950ad7783a0cf0e6acc722f2aad27fcd296c5e60
jding-tlnd <jding@talend.com>
2021-03-04 01:57:25 +0800

    fix(TUP-30169):adapter the tjdbc bulk components in TDI-45487 for studio and support singlestore database only with a whitelist https://jira.talendforge.org/browse/TUP-30169
    Signed-off-by: jding-tlnd <jding@talend.com>

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/TalendEditorDropTargetListener.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/TalendEditorDropTargetListener.java
@@ -21,7 +21,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.draw2d.IFigure;
@@ -2001,11 +2000,6 @@ public class TalendEditorDropTargetListener extends TemplateTransferDropTargetLi
 
         neededComponents = (List<IComponent>) ComponentUtilities.filterVisibleComponents(neededComponents);
 
-        RepositoryComponentSetting settingCopy = new RepositoryComponentSetting();
-        settingCopy.setInputComponent(rcSetting.getInputComponentName());
-        settingCopy.setOutputComponent(rcSetting.getOutPutComponentName());
-        settingCopy.setDefaultComponent(rcSetting.getDefaultComponentName());
-
 
         RepositoryComponentSetting compsetting = null;
         String typeName = null;
@@ -2017,22 +2011,28 @@ public class TalendEditorDropTargetListener extends TemplateTransferDropTargetLi
                 DatabaseConnection dbconn = (DatabaseConnection) connection;
                 if (UnifiedComponentUtil.isAdditionalJDBC(dbconn.getProductId())) {
                     typeName = dbconn.getProductId();
+                    String componentKey = UnifiedComponentUtil.getAdditionalJDBC().get(typeName).getComponentKey();
                     compsetting = new RepositoryComponentSetting();
                     compsetting.setInputComponent(
-                            rcSetting.getInputComponentName().replaceFirst("JDBC", StringUtils.deleteWhitespace(typeName)));
+                            rcSetting.getInputComponentName().replaceFirst("JDBC", componentKey));
                     compsetting.setOutputComponent(
-                            rcSetting.getOutPutComponentName().replaceFirst("JDBC", StringUtils.deleteWhitespace(typeName)));
+                            rcSetting.getOutPutComponentName().replaceFirst("JDBC", componentKey));
                     compsetting.setDefaultComponent(
-                            rcSetting.getDefaultComponentName().replaceFirst("JDBC", StringUtils.deleteWhitespace(typeName)));
+                            rcSetting.getDefaultComponentName().replaceFirst("JDBC", componentKey));
                 }
             }
         }
+
+        RepositoryComponentSetting settingCopy = new RepositoryComponentSetting();
+        settingCopy.setInputComponent(compsetting.getInputComponentName());
+        settingCopy.setOutputComponent(compsetting.getOutPutComponentName());
+        settingCopy.setDefaultComponent(compsetting.getDefaultComponentName());
 
         neededComponents = UnifiedComponentUtil.filterUnifiedComponent(settingCopy, neededComponents, typeName);
         // Check if the components in the list neededComponents have the same category that is required by Process.
         IComponent component = chooseOneComponent(extractComponents(neededComponents), settingCopy, quickCreateInput,
                 quickCreateOutput, typeName);
-        store.component = UnifiedComponentUtil.getEmfComponent(compsetting == null ? rcSetting : compsetting, component);
+        store.component = UnifiedComponentUtil.getEmfComponent(compsetting, component);
         store.componentName = rcSetting;
     }
 
