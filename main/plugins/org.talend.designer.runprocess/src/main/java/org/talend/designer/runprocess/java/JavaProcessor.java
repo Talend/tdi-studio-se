8317ee47328c0589c81beb05ca1c7fd36a578c42
vdrokov <vdrokov@talend.com>
2021-03-29 16:03:11 +0300

    APPINT-32452: After applying the latest patch, trunjob component fails with "java.io.IOException: Cannot run program "java.exe": error=2, No such file or directory"

--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/java/JavaProcessor.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/java/JavaProcessor.java
@@ -1329,7 +1329,7 @@ public class JavaProcessor extends AbstractJavaProcessor implements IJavaBreakpo
         final String rootWorkingDir = getRootWorkingDir(false);
 
         StringBuffer basePath = new StringBuffer(50);
-        if (isExportConfig() || isRunAsExport()) {
+        if (isExportConfig() || isRunAsExport() || isLinuxTargetPlatform()) {
             // current path.
             basePath.append('.');
 
@@ -1488,7 +1488,7 @@ public class JavaProcessor extends AbstractJavaProcessor implements IJavaBreakpo
         }
 
         StringBuffer libPath = new StringBuffer();
-        if (isExportConfig() || isRunAsExport()) {
+        if (isExportConfig() || isRunAsExport() || isLinuxTargetPlatform()) {
             boolean hasLibPrefix = libPrefixPath.length() > 0;
             for (ModuleNeeded neededModule : neededModules) {
                 if (hasLibPrefix) {
