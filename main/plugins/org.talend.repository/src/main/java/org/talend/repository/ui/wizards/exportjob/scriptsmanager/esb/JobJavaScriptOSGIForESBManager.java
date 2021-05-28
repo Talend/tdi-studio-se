34de3ba7e52dd5ef3f5cc2c16417fc13117fbe1c
jillyan <yan955599@gmail.com>
2021-05-28 11:21:14 +0800

    APPINT-32940
    correct logic and if (unselectList.size() > 0), or it will add all dependencies into manifest

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
@@ -365,28 +365,27 @@ public class JobJavaScriptOSGIForESBManager extends JobJavaScriptsManager {
                 ICamelDesignerCoreService camelService = (ICamelDesignerCoreService) GlobalServiceRegister.getDefault().getService(
                         ICamelDesignerCoreService.class);
 
-                Map<String, Boolean> unselectList = camelService.getUnselectDependenciesBundle(processItem);
-                List<URL> unselectListURLs = new ArrayList<>();
-
-                for(Set<URL> set:libResource.getAllResources()) {
-
-                    for (URL url : set) {
-
-                        boolean exist = false;
-                        for(String name: unselectList.keySet()) {
-                            if (name.equals(new File(new File(url.getFile()).toURI()).getName())) {
-                               exist = true;
-                            }
-                        }
-
-                        if (!exist) {
-                            unselectListURLs.add(url);
-                        }
-
-                    }
+				Collection<String> unselectList = camelService.getUnselectDependenciesBundle(processItem);
+				if (unselectList.size() > 0) {
+	                List<URL> unselectListURLs = new ArrayList<>();
+	
+	                for(Set<URL> set:libResource.getAllResources()) {
+	                    for (URL url : set) {
+	                        boolean exist = false;
+	                        for(String name: unselectList) {
+	                            if (name.equals(new File(new File(url.getFile()).toURI()).getName())) {
+	                               exist = true;
+	                            }
+	                        }
+	
+	                        if (!exist) {
+	                            unselectListURLs.add(url);
+	                        }
+	                    }
+	                }
+	
+	                libResourceSelected.addResources(unselectListURLs);
                 }
-
-                libResourceSelected.addResources(unselectListURLs);
             }
 
             // generate the META-INFO folder
