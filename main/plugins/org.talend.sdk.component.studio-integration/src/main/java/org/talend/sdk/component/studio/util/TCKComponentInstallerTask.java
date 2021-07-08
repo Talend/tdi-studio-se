93fd94da928ef998aecf1c0573b8939583e0e383
bhe-talendbj <bhe@talend.com>
2021-07-08 14:19:04 +0800

    feat(TUP-31487): move couchbase to bd

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/util/TCKComponentInstallerTask.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/util/TCKComponentInstallerTask.java
@@ -14,9 +14,11 @@ package org.talend.sdk.component.studio.util;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -43,7 +45,7 @@ public class TCKComponentInstallerTask extends BaseComponentInstallerTask {
      * 
      * @return car file
      */
-    protected File getCarFile() {
+    protected List<File> getCarFiles() {
         return null;
     }
 
@@ -65,16 +67,15 @@ public class TCKComponentInstallerTask extends BaseComponentInstallerTask {
     @Override
     public boolean needInstall() {
         GAV compGAV = getComponentGAV();
-        ILibraryManagerService librairesManagerService = (ILibraryManagerService) GlobalServiceRegister.getDefault().getService(
-                ILibraryManagerService.class);
+        ILibraryManagerService librairesManagerService = (ILibraryManagerService) GlobalServiceRegister.getDefault().getService(ILibraryManagerService.class);
         if (librairesManagerService != null) {
             File jarFile = librairesManagerService.resolveStatusLocally(compGAV.toMavenUri());
-            if(jarFile!=null) {
+            if (jarFile != null) {
                 log.info("Component: {} was already installed", compGAV.toString());
                 return false;
             }
         }
-        
+
         log.info("Component: {} is going to be installed", compGAV.toString());
         return true;
     }
@@ -85,26 +86,33 @@ public class TCKComponentInstallerTask extends BaseComponentInstallerTask {
             monitor = new NullProgressMonitor();
         }
         Set<File> files = new HashSet<File>();
-        File carFile = getCarFile();
-        if (carFile != null) {
-            log.info("Car file: {} was added", carFile);
-            files.add(carFile);
+        List<File> carFiles = getCarFiles();
+        if (carFiles != null) {
+            log.info("Car file: {} was added", Arrays.toString(carFiles.toArray()));
+            files.addAll(carFiles);
         }
         if (files.isEmpty()) {
             log.info("No car files");
             return false;
         }
+
+        files = files.stream().map(f -> moveFile(f)).collect(Collectors.toSet());
+
         try {
             ICarInstallationResult result = ITaCoKitUpdateService.getInstance().installCars(files, true, monitor);
-            IStatus stat = result.getInstalledStatus().get(carFile);
-            if (stat.getCode() == IStatus.OK) {
-                log.info("TCK Component installed: {}", carFile);
-                return true;
-            } else {
-                log.error("Failed to install car: {}", carFile);
+            for (File carFile : carFiles) {
+                IStatus stat = result.getInstalledStatus().get(carFile);
+                if (stat.getCode() != IStatus.OK) {
+                    log.error("Failed to install car: {}", carFile);
+                    return false;
+                }
             }
+
+            log.info("TCK Component installed: {}", Arrays.toString(carFiles.toArray()));
+            return true;
+
         } catch (Exception e) {
-            log.error("Failed to install car: {}", carFile, e);
+            log.error("Failed to install car: {}", Arrays.toString(carFiles.toArray()), e);
         }
         return false;
     }
