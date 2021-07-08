a39a572c9375e734e8ecf1c2ba7913a7de7574cf
jillyan <yan955599@gmail.com>
2021-07-08 18:22:43 +0800

    add missing import Arrays

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
@@ -12,6 +12,7 @@
 // ============================================================================
 package org.talend.repository.ui.wizards.exportjob.scriptsmanager.esb;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
